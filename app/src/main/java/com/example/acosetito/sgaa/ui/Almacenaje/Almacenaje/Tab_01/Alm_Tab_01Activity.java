dd5f0d3701e8e3e9d02fe14922f65c91ff1a8df4
a <aldair.soft@hotmail.com>
2018-09-12 12:42:23 -0500

    New activity for first tab of almacenaje. Aldair Raúl Cosetito Coral.

---
+++ app/src/main/java/com/example/acosetito/sgaa/ui/Almacenaje/Almacenaje/Tab_01/Alm_Tab_01Activity.java
@@ -0,0 +1,15 @@
+package com.example.acosetito.sgaa.ui.Almacenaje.Almacenaje.Tab_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.acosetito.sgaa.R;
+
+public class Alm_Tab_01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alm__tab_01);
+    }
+}
