6bf829fca60ae35f4b0c67c103c56ed1fd7a5854
a <aldair.soft@hotmail.com>
2018-09-21 12:21:19 -0500

    I added a interface with one method for ubication's button Aldair Raúl Cosetito Coral.

--- app/src/main/java/com/example/acosetito/sgaa/data/Adapter/Interfaces/IRVAlmTab01Adapter.java
+++ app/src/main/java/com/example/acosetito/sgaa/data/Adapter/Interfaces/IRVAlmTab01Adapter.java
@@ -1,4 +1,7 @@
 package com.example.acosetito.sgaa.data.Adapter.Interfaces;
 
+import com.example.acosetito.sgaa.data.Model.Almacenaje.UbicacionTransito;
+
 public interface IRVAlmTab01Adapter {
+    void onUbicarItem(UbicacionTransito ent);
 }
