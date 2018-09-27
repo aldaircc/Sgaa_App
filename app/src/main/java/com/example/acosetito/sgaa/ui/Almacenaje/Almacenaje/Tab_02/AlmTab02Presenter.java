2a3aa33b0dc9d4fdd5860cd7f9bb35fc37999977
a <aldair.soft@hotmail.com>
2018-09-27 10:41:08 -0500

    New methods for consuming AlmacenjaService.svc. Aldair Raúl Cosetito Coral.

---
+++ app/src/main/java/com/example/acosetito/sgaa/ui/Almacenaje/Almacenaje/Tab_02/AlmTab02Presenter.java
@@ -0,0 +1,6 @@
+package com.example.acosetito.sgaa.ui.Almacenaje.Almacenaje.Tab_02;
+
+public interface AlmTab02Presenter {
+    void validateUATransito(String strUA, Integer intIdUbicacion);
+    void navigateToTab01();
+}
